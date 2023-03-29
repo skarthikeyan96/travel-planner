dd8b78691268af0098525aad5f7e4835453c24c6
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-03-29 22:07:55 +0530

    fix: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-
 export default function Home() {
   return (
     <main>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <header className="text-gray-600 body-font border-b-2 shadow-sm">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+         
+            <span className="ml-3 text-xl">Travel Planner</span>
+          </a>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900">First Link</a>
+           
+          </nav>
+       
+        </div>
+      </header>
     </main>
-  )
+  );
 }
